400870b329bae5ba485ce0bec996c1192d4ac46b
Dallas Read <dallas@excitereative.ca>
2014-02-02 22:52:58 -0400

    test.

--- db/migrate/20140202202011_update_counter_cache_for_users.rb
+++ db/migrate/20140202202011_update_counter_cache_for_users.rb
@@ -1,7 +1,7 @@
 class UpdateCounterCacheForUsers < ActiveRecord::Migration
   def change
     User.find_each do |u|
-      u.update_columns contacts_count: u.contacts.count
+      User.update(u.id, contacts_count: u.contacts.count)
     end
   end
 end
