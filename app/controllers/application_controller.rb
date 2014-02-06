c918620d766d550cf49e14f1209ccf1836d1e8d2
Dallas Read <dallas@excitereative.ca>
2014-02-06 16:12:54 -0400

    Doesn't authenticate with img.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  skip_before_filter :verify_authenticity_token, if: Proc.new { request.format == :json }
-  before_filter :authenticate_user_from_api_key!, if: Proc.new { (params[:api_key] || params[:remetric_api_key]) && request.format == :json }
-  before_filter :authenticate_user!
+  skip_before_filter :verify_authenticity_token, if: :api_hit?
+  before_filter :authenticate_user_from_api_key!, if: :api_hit?
+  before_filter :authenticate_user!, unless: :img_hit?
   before_filter :set_user
-  around_filter :set_time_zone, if: Proc.new { @user }
+  around_filter :set_time_zone
   before_filter :set_time_cookies
   
   private
   
+  def api_hit?
+    params[:api_key] && request.format == :json
+  end
+  
+  def img_hit?
+    action_name == "img"
+  end
+  
   def set_time_zone(&block)
-    Time.use_zone(@user.time_zone, &block)
+    Time.use_zone(@user.time_zone, &block) if @user
   end
   
   def set_time_cookies
-    cookies[:now_offset] = { value: params[:now_offset] || 0, expires: 1.day.from_now } if params[:now_offset]
+    cookies[:now_offset] = { value: params[:now_offset] || 0, expires: 1.day.from_now } if @user && params[:now_offset]
   end
   
   def set_times
@@ -27,8 +35,8 @@ class ApplicationController < ActionController::Base
   end
   
   def authenticate_user_from_api_key!
-    if /(contacts\#(save|overwrite)|events\#(create|img))/ =~ "#{controller_name}##{action_name}"
-      api_key = params[:remetric_api_key] ? params[:remetric_api_key] : params[:api_key]
+    if /(contacts\#(save|overwrite)|events\#(create))/ =~ "#{controller_name}##{action_name}"
+      api_key = params[:api_key]
       user = User.where(api_key: api_key).first
       sign_in user, store: false if user
     else
